0a34c12af03f6c058f1f79aa2df3088705cee20d
unruh043 <58611110+unruh043@users.noreply.github.com>
2019-12-06 17:14:20 -0600

    Add files via upload

--- Simple_Mars_Reentry.m
+++ Simple_Mars_Reentry.m
@@ -2,12 +2,12 @@
 %Define constants
 Radius_Mars = 3390;                     %(km)
 Mass_Mars   = 6.39e23;                  %(kg)
-Mass_craft  = 10.4;                     %(kg)
-G           = (6.67408e-11);            %(km^3/(kg*s^2))
+Mass_craft  = 10400;                    %(kg)
+G           = (6.67408e-11)/1000^3;     %(km^3/(kg*s^2))
 
 %Define initial conditions
-Altitude = Radius_Mars + 200;             %(km)
-V0 = 11.115490396972195;                  %(km/s)
+Altitude = Radius_Mars + 200;           %(km)
+V0 = sqrt(G*(Mass_Mars)/Altitude);                              %(km/s)
 
 %Define initial state vector and time vector
 PosState = [Altitude;0;0];              %[x;y;z]   (km;km;km)
@@ -15,7 +15,7 @@ VelState = [0;V0;0];                    %[Vx;Vy;Vz](km/s;km/s;km/s;)
 
 state0 = [PosState; VelState];
 
-time = linspace(1,9000,9e4);
+time = linspace(1,6600,9e4);
 
 %Calculating orbit
 orbit = ODENumIntRK4(@CraftOrbit,time,state0,Mass_Mars,Mass_craft,G);
