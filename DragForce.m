7824dd3360da56aba8dae88c8307513ccfb7c9dc
nels9246 <58611109+nels9246@users.noreply.github.com>
2019-12-06 17:49:15 -0600

    Add files via upload

--- DragForce.m
+++ DragForce.m
@@ -1,4 +1,4 @@
-function drag = DragForce(rho,B,m,A,v)
+function drag = DragForce(rho,Cd,A,v)
 
 %Drag Force 
 %Calculates the drag force of the spacecraft depending on the current
@@ -10,17 +10,15 @@ function drag = DragForce(rho,B,m,A,v)
 %
 %Inputs:
 %   rho: atmospheric density    [kg/m^3]
-%   B: ballistic coefficient    [kg/m^2]
-%   m: mass                     [kg]
+%   Cd: drag coefficient        [kg/m^2]
 %   A: cross-sectional area     [m^2]
 %   v: velocity                 [km/s]
 %
 %Outputs:
 %   drag: drag force            [N]         
 
+%convert to m/s
 v=v*1000;
-
-Cd=m/(B*A);
 
 drag=1/2*rho*Cd*A*v.^2;
 
